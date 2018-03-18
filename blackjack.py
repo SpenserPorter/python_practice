951afc49e68a9ce6908b831f23667f87b4c4ea3b
Spenser Porter <Spenser.Porter@gmail.com>
2018-03-18 13:13:09 -0400

    finished implimenting multiplayer without split, double down

--- blackjack.py
+++ blackjack.py
@@ -87,6 +87,8 @@ class Hand(list):
         for unused in range(0,ace_count):
             if total > 21:
                 total -= 10
+        if total > 21:
+            self.is_bust = True
         return total
 
     def is_blackjack(self):
@@ -141,11 +143,12 @@ class Round(object):
     def __init__(self, shoe, players):
         self.shoe = shoe
         self.ledger = {}
+        self.game_state = 1
         for player in players:
             self.add_player_to_round(player)
         Round.round_id += 1
 
-    def add_player_to_round(self, player): #add player to ledger with with 1st wager/hand
+    def add_player_to_round(self, player):  # add player to ledger with with 1st wager/hand
         self.ledger[player.name] = None
 
     def get_players(self):
@@ -168,8 +171,20 @@ class Round(object):
         else:
             self.ledger[player.name][hand_index].set_hand(Hand(self.shoe.draw_cards(2)))
 
-    def get_player_hand(self, player_name, hand_index = 0):
-        return self.ledger[player_name][hand_index].get_hand()
+    def get_player_hand(self, player, hand_index = 0):
+        return self.ledger[player][hand_index].get_hand()
+
+    def get_result(self, player):
+        player_value = self.get_player_hand(player).get_value()
+        dealer_value = self.get_player_hand("Dealer").get_value()
+        if self.get_player_hand(player).is_bust:
+            return False
+        if player_value == dealer_value:
+            return "Push"
+        elif player_value < dealer_value:
+            return "Lose"
+        elif player_value > dealer_value:
+            return "Win"
 
 
 class Menu(object):
@@ -179,39 +194,39 @@ class Menu(object):
 
     def menu(self, player):
         loop = True
-        while loop:  ## While loop which will keep going until loop = False
-            self.print_menu(player)  ## Displays menu
+        while loop:  # While loop which will keep going until loop = False
+            self.print_menu(player)  # Displays menu
             choice = input("Enter your choice: ")
-            if choice in ["1","h"]:
+            if choice in ["1","h","H"]:
                 return "Hit"
-            elif choice in ["2","s"]:
+            elif choice in ["2","s","S"]:
                 return "Stand"
-            elif choice in ["3", "d"]:
+            elif choice in ["3", "d","D"]:
                 return "Double"
             elif choice == 4:
                 return "Split"
             elif choice == 5:
                 return "Surrender"
-            loop = False  # This will make the while loop to end as not value of loop is set to False
-
+            loop = False  # This will make the while loop end
 
 
     def print_menu(self, player):
 
-        print(30 * "-", player.name + "'s", " Action", 30 * "-")
+        print(30 * "-", player.name + " Action", 30 * "-")
         print("1. Hit")
         print("2. Stand")
         if self.state == 1:
-            print("3. Double")
+            print("3. Double Down!")
+            self.state = 3
         # print("4. Surrender")
         # print("5. Split")
-        print(67 * "-")
+        print((72 + len(player.name)) * "-")
 
 class Game(object):
 
     def __init__(self, number_of_players):
         self.shoe = Shoe(6)
-        self.players = [Player("Dealer", True)]
+        self.players = []
         self.initialize_player_list(number_of_players)
         self.current_round = Round(self.shoe, self.players)
 
@@ -219,46 +234,134 @@ class Game(object):
         for i in range(1, number_of_players + 1):
             name = "Player" + str(i)
             self.players.append(Player(name))
+        self.players.append(Player("Dealer", True))
+
+    def next_round(self, shoe, players):
+        self.current_round = Round(shoe, players)
+        self.start()
 
     def start(self):
 
-        print ("Round", self.current_round.round_id)
+        while True:
 
-        for player in self.players:
+            print ("Round", self.current_round.round_id)
 
-            wager = 0
+            for player in self.players:
 
-            if not player.is_dealer:
+                wager = 0
 
-                print (player.name + "'s balance is " + str(player.balance))
-                place_bet = input("Place your bet " + player.name + ":")
+                if not player.is_dealer:
 
-                while True: #validate bet is an integer between 1 and total balance
-                    try:
-                        wager = int(place_bet)
-                        if wager in range(1, player.get_balance() + 1):
+                    print ("%s balance is %i" % (player.name, player.balance))
+                    place_bet = input("Place your bet " + player.name + ":")
+
+                    while True: #validate bet is an integer between 1 and total balance
+                        try:
+                            wager = int(place_bet)
+                            if wager in range(1, player.get_balance() + 1):
+                                break
+                            place_bet = input("Please enter a valid bet " + player.name + ":")
+                        except ValueError:
+                            place_bet = input("Please enter a valid bet " + player.name + ":")
+
+                    player.modify_balance(-1 * wager)
+
+                self.current_round.ledger[player.name] = [HandWagerPair(Hand(self.current_round.shoe.draw_cards(2)), wager)]
+
+
+            for player in self.players:
+                if not player.is_dealer:
+                    first_decision = True
+                    #check if player can double based on wager/balance
+                    if player.get_balance() >= self.current_round.get_player_wager(player):
+                        self.current_round.game_state = 1
+                    else:
+                        self.current_round.game_state = 2
+
+                    while True:
+                        if first_decision:
+                            print("Dealer shows: %s X" % (self.current_round.get_player_hand("Dealer").get_cards(0)))
+                            print("%s's hand: %s %i total" % (player.name, self.current_round.get_player_hand(player.name), self.current_round.get_player_hand(player.name).get_value()))
+                            first_decision = False
+
+
+                        if self.current_round.get_player_hand(player.name).is_blackjack():
+                            print ("%s Blackjack!, Win %f!" % (player.name, 2.5 * self.current_round.get_player_wager(player)))
+                            player.modify_balance(2.5 * self.current_round.get_player_wager(player))
                             break
-                        place_bet = input("Please enter a valid bet " + player.name + ":")
-                    except ValueError:
-                        place_bet = input("Please enter a valid bet " + player.name + ":")
 
-                player.modify_balance(-1 * wager)
+                        action = Menu(self.current_round.game_state).menu(player)
 
-            self.current_round.ledger[player.name] = [HandWagerPair(Hand(self.current_round.shoe.draw_cards(2)), wager)]
+                        if action == "Hit" or action == "Double":
+                            self.current_round.game_state = 2
+                            self.current_round.get_player_hand(player.name).add_cards(self.current_round.shoe.draw_cards(1))
+                            if not self.current_round.get_result(player.name):
+                                print("Dealer shows: %s X" % (self.current_round.get_player_hand("Dealer").get_cards(0)))
+                                print("%s's hand: %s %i total" % (player.name, self.current_round.get_player_hand(player.name), self.current_round.get_player_hand(player.name).get_value()))
+                                print("%s Bust!" % (player.name))
+                                break
+                            else:
+                                if action == "Double":
+                                    player.modify_balance(-1 * self.current_round.get_player_wager(player))
+                                    self.current_round.ledger[player.name][0].set_wager(self.current_round.get_player_wager(player) * 2)
+                                    print("%s doubles to %i" %(player.name,self.current_round.get_player_wager(player)))
+                                    print("%s hand: %s %i total" % (player.name, self.current_round.get_player_hand(player.name), self.current_round.get_player_hand(player.name).get_value()))
+                                    break
+                                else:
+                                    print("Dealer shows: %s X" % (self.current_round.get_player_hand("Dealer").get_cards(0)))
+                                    print("%ss hand: %s %i total" % (player.name, self.current_round.get_player_hand(player.name), self.current_round.get_player_hand(player.name).get_value()))
+
+                        if action == "Stand":
+                            break
+
+            #Start dealer logic    
+
+            while self.current_round.get_player_hand("Dealer").get_value() < 17:
+
+                self.current_round.get_player_hand("Dealer").add_cards(self.current_round.shoe.draw_cards(1))
+                print("Dealer draws 1 and shows %s, %i total" % (self.current_round.get_player_hand(player.name), self.current_round.get_player_hand(player.name).get_value()))
+
+            if self.current_round.get_player_hand("Dealer").is_bust:
+                print("Dealer busts!")
+                for player in self.players:
+                    if not self.current_round.get_player_hand(player.name).is_bust and not player.is_dealer:
+                        print("%s wins %i!" % (player.name, 2 * self.current_round.get_player_wager(player)))
+                        player.modify_balance(2 * self.current_round.get_player_wager(player))
 
 
-        for player in self.players:
-            if not player.is_dealer:
-                game_state = 1
-                print("Dealer shows: %s X" % (self.current_round.get_player_hand("Dealer").get_cards(0)))
-                print("%s's hand: %s %i total" % (player.name, self.current_round.get_player_hand(player.name), self.current_round.get_player_hand(player.name).get_value()))
+            else:
 
-                if self.current_round.get_player_hand(player.name).is_blackjack():
-                    print (player.name, "Blackjack!")
-                action = Menu(game_state).menu(player)
+                print("Dealer shows %s %i total" % (self.current_round.get_player_hand("Dealer"),self.current_round.get_player_hand("Dealer").get_value()) )
 
-                if action == "Hit":
-                    game_state = 2
+                for player in self.players:
+                    if not self.current_round.get_player_hand(player.name).is_bust and not player.is_dealer and not self.current_round.get_player_hand(player.name).is_blackjack:
+                        result = self.current_round.get_result(player.name)
+                        if result == "Push":
+                            print("%s %s %i total, Pushes, wager %i returned" % (player.name, self.current_round.get_player_hand(player.name),self.current_round.get_player_hand(player.name).get_value(), self.current_round.get_player_wager(player)))
+                            player.modify_balance(self.current_round.get_player_wager(player))
+                        elif result == "Win":
+                            print("%s %s %i total, Wins %i!" % (player.name, self.current_round.get_player_hand(player.name),self.current_round.get_player_hand(player.name).get_value(), 2 * self.current_round.get_player_wager(player)))
+                            player.modify_balance(2 * self.current_round.get_player_wager(player))
+                        else:
+                            print("%s %s %i total, Loses %i shit!!" % (player.name, self.current_round.get_player_hand(player.name),self.current_round.get_player_hand(player.name).get_value(), self.current_round.get_player_wager(player)))
+
+            self.next_round(self.shoe, self.players)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
